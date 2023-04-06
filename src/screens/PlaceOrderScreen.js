67658d0ed80a2ec860512f97f4e84bcb8f3e28a4
Muhammad Afiq Bin Samsol Badron <afiqsam71@gmail.com>
2023-04-06 15:24:46 +0800

    video-27

--- src/screens/PlaceOrderScreen.js
+++ src/screens/PlaceOrderScreen.js
@@ -1,5 +1,138 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
+import Card from 'react-bootstrap/esm/Card';
+import ListGroup from 'react-bootstrap/esm/ListGroup';
+import { Helmet } from 'react-helmet-async';
+import CheckoutSteps from '../Components/CheckoutSteps';
+import { Store } from '../Store';
+import { Link, useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/esm/Button';
 
 export default function PlaceOrderScreen() {
-  return <div></div>;
+  const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100; // 123.2345 => 123.23
+  cart.itemsPrice = round2(
+    cart.cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
+  );
+  cart.shippingPrice = cart.itemsPrice > 100 ? round2(0) : round2(10);
+  cart.taxPrice = round2(0.15 * cart.itemsPrice);
+  cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+
+  const placeOrderHandler = () => {};
+
+  useEffect(() => {
+    if (!cart.paymentMethod) {
+      navigate('/payment');
+    }
+  }, [cart, navigate]);
+
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
+      <Helmet>
+        <title>Preview Order</title>
+      </Helmet>
+      <h1 className="my-3">Place Order</h1>
+      <Row>
+        <Col md={8}>
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>Shipping</Card.Title>
+              <Card.Text>
+                <strong>Name : </strong> {cart.shippingAddress.fullName} <br />
+                <strong> Address : </strong> {cart.shippingAddress.address},{' '}
+                {cart.shippingAddress.city}, {cart.shippingAddress.postalCode},{' '}
+                {cart.shippingAddress.country}
+              </Card.Text>
+              <Link to="/shipping">Edit</Link>
+            </Card.Body>
+          </Card>
+
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>Payment</Card.Title>
+              <Card.Text>
+                <strong>Method : </strong> {cart.paymentMethod}
+              </Card.Text>
+              <Link to={'/payment'}>Edit</Link>
+            </Card.Body>
+          </Card>
+
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>Items</Card.Title>
+              <ListGroup variant="flush">
+                {cart.cartItems.map((item) => (
+                  <ListGroup.Item key={item._id}>
+                    <Row className="align-item-center">
+                      <Col md={6}>
+                        <img
+                          src={item.image}
+                          alt={item.image}
+                          className="img-fluid rounded img-thumbnail"
+                        ></img>{' '}
+                        <Link to={`/product/${item.slug}`}>{item.name}</Link>
+                      </Col>
+                      <Col md={3}>
+                        <span>{item.quantity}</span>
+                      </Col>
+                      <Col md={3}>
+                        <span>RM {item.price}</span>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card.Body>
+            <Card.Title>Order Summary</Card.Title>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>RM {cart.itemsPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>RM {cart.shippingPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>GST Tax (6%)</Col>
+                  <Col>RM {cart.taxPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Order Total</Col>
+                  <Col>RM {cart.totalPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+            <ListGroup.Item>
+              <div className="d-grid">
+                <Button
+                  type="button"
+                  onClick={placeOrderHandler}
+                  disabled={cart.cartItems.length === 0}
+                >
+                  Place Order
+                </Button>
+              </div>
+            </ListGroup.Item>
+          </Card.Body>
+        </Col>
+      </Row>
+    </div>
+  );
 }
