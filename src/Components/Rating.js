534b4d6149d2f5f303116679339dc1b13cee2435
Muhammad Afiq Bin Samsol Badron <afiqsam71@gmail.com>
2023-06-28 13:48:50 +0800

    video-35

--- src/Components/Rating.js
+++ src/Components/Rating.js
@@ -1,5 +1,6 @@
 function Rating(props) {
-  const { rating, numReviews } = props;
+  // const { rating, numReviews } = props;
+  const { rating, numReviews, caption } = props;
   return (
     <div className="rating">
       <span>
@@ -57,7 +58,12 @@ function Rating(props) {
           }
         />
       </span>
-      <span> {numReviews} reviews</span>
+      {/* <span> {numReviews} reviews</span> */}
+      {caption ? (
+        <span>{caption}</span>
+      ) : (
+        <span>{' ' + numReviews + ' reviews'}</span>
+      )}
     </div>
   );
 }
